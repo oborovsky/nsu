1e11db8eeb730e38005ebc9191c667bd34d2c21b
doborovsky <doborovsky@gmail.com>
2015-02-19 17:26:26 +0600

    add lesson2

---
+++ lesson2/prime.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define N 5
+
+int isPrime(int n)
+{
+
+}
+int main()
+{
+	int arr[N]= {1,2,3,4,5};
+	int i, j, cur;
+	int a = 0;
+	int b = 0;
+
+	for(i = 0; i < N; i++)
+	{
+		cur = arr[i];
+		for(j=cur; j > 0; j--)
+		{
+			if( (cur%j) == 0 )
+		}
+	}
+}
