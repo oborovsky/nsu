5e73bade89df837594a762adde28c78143a85621
doborovsky <doborovsky@gmail.com>
2015-02-26 17:20:41 +0600

    rewrite mscanf

--- mscanf.c
+++ mscanf.c
@@ -2,40 +2,28 @@
 #define K 100
 int isNumber(int n)
 {
-	//printf("in isNumber =%c,%d\n",n,n);
-	int N[10] = {'0','1','2','3','4','5','6','7','8','9'}; 
-	int key = -1;
-	int i = 0;
-	for(; i < 10; i++ )
-	{	//printf("n=%d, Ni=%d\n",n,N[i] );
-		if( n == N[i])
-		{
-			key = 1; 
-			break;
-		}
-	}
-	//printf("key= %d\n",key );
-	return key;
+	int nul = '0';
+	n -= '0';
+	if( n >= 0 && n < 10) return 1;
+	return 0;
 }
 int mscanf(int* num)
 {
 	int n = getchar();
-	int key = 1;
 	int i=0;
 	*num = 0;
-	for( ; n != '\n'; )
+	for( ; n != '\n'|| i == 0; )
 	{
 		//printf("read%c\n",n);
 		if( isNumber(n) == 1) 
 		{
 			*num = (*num)*10 + (n - '0');
-			key = 2;
 			i++;
 			//printf("in isNumber num = %d\n",*num );
 		}
 		else 
 		{
-			if (key == 2)
+			if (i > 0)
 			{
 				break;
 			}
@@ -46,12 +34,9 @@ int mscanf(int* num)
 }
 int main()
 {
-	int num;
+	int num = 0;
 	int i = mscanf(&num);
 	if( i > 0) printf("%d\n", num );
-	else 
-	{
-		//printf("There is no number.\n");
-	}
 	return 0;
+	
 }
