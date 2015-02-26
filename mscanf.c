4796cf4510ce7568a3768df5f1aa6584a4ef7a99
oborovsky <doborovsky@gmail.com>
2015-02-26 23:54:20 +0600

    clean mscanf

--- mscanf.c
+++ mscanf.c
@@ -2,7 +2,6 @@
 #define K 100
 int isNumber(int n)
 {
-	int nul = '0';
 	n -= '0';
 	if( n >= 0 && n < 10) return 1;
 	return 0;
@@ -14,12 +13,10 @@ int mscanf(int* num)
 	*num = 0;
 	for( ; n != '\n'|| i == 0; )
 	{
-		//printf("read%c\n",n);
 		if( isNumber(n) == 1) 
 		{
 			*num = (*num)*10 + (n - '0');
 			i++;
-			//printf("in isNumber num = %d\n",*num );
 		}
 		else 
 		{
